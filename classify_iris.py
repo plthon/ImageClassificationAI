803dd35e71f3859ad922f5b2ab9a9b01010d9a99
bnthnix <101209471+bnthnix@users.noreply.github.com>
2019-11-05 10:17:41 +0800

    Progress.

--- classify_iris.py
+++ classify_iris.py
@@ -16,3 +16,49 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--model", type=str, default="knn",
                 help="type of python machine learning model to use")
 args = vars(ap.parse_args())
+
+# define the dictionary of models our script can use, where the key
+# to the dictionary is the name of the model (supplied via command
+# line argument) and the value is the model itself
+models = {
+	"knn": KNeighborsClassifier(n_neighbors=1),
+	"naive_bayes": GaussianNB(),
+	"logit": LogisticRegression(solver="lbfgs", multi_class="auto"),
+	"svm": SVC(kernel="rbf", gamma="auto"),
+	"decision_tree": DecisionTreeClassifier(),
+	"random_forest": RandomForestClassifier(n_estimators=100),
+	"mlp": MLPClassifier()
+}
+
+# define the dictionary of models our script can use, where the key
+# to the dictionary is the name of the model (supplied via command
+# line argument) and the value is the model itself
+models = {
+	"knn": KNeighborsClassifier(n_neighbors=1),
+	"naive_bayes": GaussianNB(),
+	"logit": LogisticRegression(solver="lbfgs", multi_class="auto"),
+	"svm": SVC(kernel="rbf", gamma="auto"),
+	"decision_tree": DecisionTreeClassifier(),
+	"random_forest": RandomForestClassifier(n_estimators=100),
+	"mlp": MLPClassifier()
+}
+
+# load the Iris dataset and perform a training and testing split,
+# using 75% of the data for training and 25% for evaluation
+print("[INFO] loading data...")
+dataset = load_iris()
+(trainX, testX, trainY, testY) = train_test_split(dataset.data,
+                                                  dataset.target,
+                                                  random_state=3,
+                                                  test_size=0.25)
+
+# train the model
+print("[INFO] using '{}' model".format(args["model"]))
+model = models[args["model"]]
+model.fit(trainX, trainY)
+
+# make predictions on our data and show a classification report
+print("[INFO] evaluating...")
+predictions = model.predict(testX)
+print(classification_report(testY, predictions,
+                            target_names=dataset.target_names))

8b4246e0d838798da18a36cd9ec50ec0100fb512
plthon <plthon@outlook.com>
2019-11-14 22:58:15 +0800

    Updates.

--- FINAL_MNIST.py
+++ FINAL_MNIST.py
@@ -12,7 +12,8 @@ import seaborn as sns
 from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, hamming_loss, jaccard_score, multilabel_confusion_matrix
+from sklearn.metrics import classification_report, hamming_loss, jaccard_score, multilabel_confusion_matrix, \
+    accuracy_score, log_loss, roc_auc_score, coverage_error, average_precision_score
 from sklearn.metrics import confusion_matrix, zero_one_loss
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
@@ -23,45 +24,45 @@ from sklearn.tree import DecisionTreeClassifier
 
 startTime = time.time()
 
+# --- You wont use this. --- Redundant codes. -------------------------------------------------------------------------
 ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--model", type=str, default="knn",
                 help="type of python machine learning model to use")
 args = vars(ap.parse_args())
+# ---------------------------------------------------------------------------------------------------------------------
 
-"""
+# --- DATA PREPARATION & PREPROCESSING --------------------------------------------------------------------------------
 print("[INFO] Loading data...")
-# load the MNIST digits dataset
-mnistZ = datasets.load_digits()
-# take the MNIST data and construct the training and testing split, using 75% of the data for training and 25% for
-# testing
-(trainData, testData, trainLabels, testLabels) = train_test_split(np.array(mnistZ.data),
-                                                                  mnistZ.target, test_size=0.25, random_state=42)
-
-# now, let's take 10% of the training data and use that for validation
-(trainData, valData, trainLabels, valLabels) = train_test_split(trainData, trainLabels,
-                                                                test_size=0.1, random_state=84)
-"""
-
-# METHOD TWO
-print("[INFO] Loading data...")
+loadTime = time.time()
+# Load dataset, Produce trainData, testData, trainLabels, testLabels
+# TODO: Change the path to appropriate path on your device
 mnist = MNIST('/home/plthon/PycharmProjects/ImageClassificationAI/dataset/MNIST/')
-trainData, trainLabels = mnist.load_training() #60000 samples
-testData, testLabels = mnist.load_testing()    #10000 samples
-
+trainData, trainLabels = mnist.load_training()  # 60000 samples
+testData, testLabels = mnist.load_testing()     # 10000 samples
+# Concatenate to enable custom train size and test size for whatever purpose
+x = np.concatenate((trainData, testData))
+y = np.concatenate((trainLabels, testLabels))
+# TODO: Smaller train size and test size, faster program execution, lower accuracy score
+train_size = 600    # Default: 60000
+test_size = 100     # Default: 10000
+trainData, testData, trainLabels, testLabels = train_test_split(x, y, train_size=train_size, test_size=test_size,
+                                                                random_state=42)
+# It is most common to use 32-bit precision when training a neural network, so at one point the training data will
+# have to be converted to 32 bit floats. Since the dataset fits easily in RAM, we might as well convert to float
+# immediately. (Aurélien 2018)
 trainData = np.asarray(trainData).astype(np.float32)
 trainLabels = np.asarray(trainLabels).astype(np.int32)
 testData = np.asarray(testData).astype(np.float32)
 testLabels = np.asarray(testLabels).astype(np.int32)
-# METHOD TWO
-
 # show the sizes of each data split
-print("training data points: {}".format(len(trainLabels)))
-# print("validation data points: {}".format(len(valLabels)))
-print("testing data points: {}".format(len(testLabels)))
+print("Training data points: {}".format(len(trainLabels)))
+print("Testing data points: {}".format(len(testLabels)))
+print("Time used (seconds):", datetime.timedelta(seconds=time.time() - loadTime))
+# ---------------------------------------------------------------------------------------------------------------------
 
-# define the dictionary of models our script can use, where the key
-# to the dictionary is the name of the model (supplied via command
-# line argument) and the value is the model itself
+# --- TRAINING --------------------------------------------------------------------------------------------------------
+# define the dictionary of models our script can use, where the key to the dictionary is the name of the model
+# (supplied via command line argument) and the value is the model itself
 models = {
     "knn": KNeighborsClassifier(n_neighbors=1),
     "naive_bayes": GaussianNB(),
@@ -72,22 +73,23 @@ models = {
     "mlp": MLPClassifier()
 }
 
+# TODO: Select models from selections above
 # chosenModel = args["model"]
-chosenModel = "knn"
+chosenModel = "mlp"
 
 print("\n[INFO] Using '{}' model".format(chosenModel))
 fitTime = time.time()
 model = models[chosenModel]
 model.fit(trainData, trainLabels)
 print("Time used (seconds):", datetime.timedelta(seconds=time.time() - fitTime))
+# ---------------------------------------------------------------------------------------------------------------------
 
+# --- Evaluation ------------------------------------------------------------------------------------------------------
 print("\n[INFO] Evaluating...")
 preditTime = time.time()
 predictions = model.predict(testData)
+predictions2 = model.predict_proba(testData)
 print("Time used (seconds):", datetime.timedelta(seconds=time.time() - preditTime))
-
-# predictions2 = model.predict_proba(testData)
-# print(predictions[1])
 
 # show a final classification report demonstrating the accuracy of the classifier
 # for each of the digits
@@ -106,12 +108,11 @@ print("\nHamming Loss:", hamming_loss(testLabels, predictions))
 print("\nJaccard Score:", jaccard_score(testLabels, predictions, average=None))
 print("\nMultiLabel Confusion Matrix:")
 print(multilabel_confusion_matrix(testLabels, predictions))
-# Average Precision Score
-# Log Loss
-# Roc Auc Score
-# Coverage Error
+# print("\nAverage Precision Score:", average_precision_score(testLabels, predictions2))
+print("\nLog Loss:", log_loss(testLabels, predictions2))
+# print("\nRoc Auc Score:", roc_auc_score(testLabels, predictions2))
+# print("\nCoverage Error:", coverage_error(testLabels, predictions2))
 
-"""
 print()
 # loop over a few random digits
 for i in np.random.randint(0, high=len(testLabels), size=(5,)):
@@ -120,11 +121,12 @@ for i in np.random.randint(0, high=len(testLabels), size=(5,)):
     prediction = model.predict([image])[0]
     # show the prediction
     imgData = np.array(image, dtype='float')
-    pixels = imgData.reshape((8, 8))
+    pixels = imgData.reshape((28, 28))
     plt.imshow(pixels, cmap='gray')
     plt.annotate(prediction, (3, 3), bbox={'facecolor': 'white'}, fontsize=16)
     print("I think tha digit is : {}".format(prediction))
     plt.show()
     cv2.waitKey(0)
-"""
+
+# Show total time program execution.
 print("\n\n\nTotal Time Taken:", datetime.timedelta(seconds=time.time() - startTime))
--- FINAL_SVHN.py
+++ FINAL_SVHN.py
@@ -8,7 +8,7 @@ import seaborn as sns
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, confusion_matrix, zero_one_loss, hamming_loss, \
-    jaccard_score, multilabel_confusion_matrix
+    jaccard_score, multilabel_confusion_matrix, log_loss
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
@@ -19,12 +19,16 @@ from sklearn.utils import shuffle
 
 startTime = time.time()
 
+# --- You wont use this. --- Redundant codes. ---
 ap = argparse.ArgumentParser()
 ap.add_argument("-m", "--model", type=str, default="knn",
                 help="type of python machine learning model to use")
 args = vars(ap.parse_args())
+# ---------------------------------------------------------------------------------------------------------------------
 
+# --- DATA PREPARATION & PREPROCESSING --------------------------------------------------------------------------------
 print("[INFO] Loading data...")
+loadTime = time.time()
 # load our dataset
 # dataset link: http://ufldl.stanford.edu/housenumbers/
 train_data = scipy.io.loadmat('dataset/train_32x32.mat')
@@ -40,16 +44,17 @@ print(y[img_index])
 X = X.reshape(X.shape[0] * X.shape[1] * X.shape[2], X.shape[3]).T
 y = y.reshape(y.shape[0], )
 X, y = shuffle(X, y, random_state=42)
-
+print(X.shape)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 print('Train data shape: ', X_train.shape)
 print('Train labels shape: ', y_train.shape)
 print('Test data shape: ', X_test.shape)
 print('Test labels shape: ', y_test.shape)
-
+print("Time used (seconds):", datetime.timedelta(seconds=time.time() - loadTime))
 # ---------------------------------------------------------------------------------------------------------------------
 
+# --- TRAINING --------------------------------------------------------------------------------------------------------
 models = {
     "knn": KNeighborsClassifier(n_neighbors=1),
     "naive_bayes": GaussianNB(),
@@ -68,10 +73,13 @@ fitTime = time.time()
 model = models[chosenModel]
 model.fit(X_train, y_train)
 print("Time used (seconds):", datetime.timedelta(seconds=time.time() - fitTime))
+# ---------------------------------------------------------------------------------------------------------------------
 
+# --- Evaluation ------------------------------------------------------------------------------------------------------
 print("\n[INFO] Evaluating...")
 preditTime = time.time()
 predictions = model.predict(X_test)
+predictions2 = model.predict_proba(testData)
 print("Time used (seconds):", datetime.timedelta(seconds=time.time() - preditTime))
 
 print("\n--- EVALUATION ON TESTING DATA ---")
@@ -90,7 +98,7 @@ print("\nJaccard Score:", jaccard_score(y_test, predictions, average=None))
 print("\nMultiLabel Confusion Matrix:")
 print(multilabel_confusion_matrix(y_test, predictions))
 # Average Precision Score
-# Log Loss
+print("\nLog Loss:", log_loss(y_test, predictions2))
 # Roc Auc Score
 # Coverage Error
 
